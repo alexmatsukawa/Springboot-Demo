ab085c6b65cc1ad126ce939135b723ed14c81397
Alex Matsukawa <amatsukawa9@gmail.com>
2021-12-11 14:56:33 -0700

    added mapping tags for restful deployment

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,22 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.*;
 
 @SpringBootApplication
+@RestController //-> this tag makes this class serve restful endpoints
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping
+	public List<String> hello() {
+		//return "Hello World!";
+		return List.of("Hello", "World");
 	}
 
 }
