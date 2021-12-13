433c1d2d73bed78e3f5e6aefec22564410f30c2c
Alex Matsukawa <amatsukawa9@gmail.com>
2021-12-12 17:23:58 -0700

    added a StudentRepository interface to handle accessing data from the PostGreSQL db; updated student service with DI from the repo class

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,5 +1,6 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -9,17 +10,27 @@ import java.util.List;
 @Service //sets this class as a Spring bean
 public class StudentService {
 
+    private final StudentRepository studentRepository;
+
+    @Autowired //Dependency Injection for Repository of Students
+    public  StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
     public List<Student> getStudents() {
         //return "Hello World!";
-        return List.of(
-                new Student(
-                        1L,
-                        "Miriam",
-                        "miriam.jamal@gmail.com",
-                        LocalDate.of(2000, Month.JANUARY, 5),
-                        21
-                )
-        );
+//        return List.of(
+//                new Student(
+//                        1L,
+//                        "Miriam",
+//                        "miriam.jamal@gmail.com",
+//                        LocalDate.of(2000, Month.JANUARY, 5),
+//                        21
+//                )
+//        );
         //this returns student data as json in the web browser on port 8080
+
+        //better way of getting a list of all students compared to above code...
+        return studentRepository.findAll(); //returns a list of all students in the db
+        //magic of Spring JPA -> don't need to implement the interface methods for a Postgres repo; methods will work automatically
     }
 }
